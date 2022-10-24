5b13f4b0215d995cf4215e864b24b90709603adc
ChoMinhyun <cho.minhyun@cashwalk.io>
2022-10-24 15:53:36 +0900

    Set Color

--- ProgressBar_Test/ProgressBar_Test/ViewController.swift
+++ ProgressBar_Test/ProgressBar_Test/ViewController.swift
@@ -15,14 +15,18 @@ class ViewController: UIViewController {
     var kcalText2: UILabel!
     var pieChartView: PieChartView!
     
+    let redColor: UIColor = UIColor.init(red: CGFloat(255/255.0), green: CGFloat(98/255.0), blue: CGFloat(126/255.0), alpha: 0.68)
+    let blueColor: UIColor = UIColor.init(red: CGFloat(31/255.0), green: CGFloat(163/255.0), blue: CGFloat(249/255.0), alpha: 0.68)
+    let greenColor: UIColor = UIColor.init(red: CGFloat(0/255.0), green: CGFloat(214/255.0), blue: CGFloat(124/255.0), alpha: 0.68)
+    
     let dummyKcal: Float = 350.5
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        presentView(kcal: dummyKcal)
+        presentCircularBarView(kcal: dummyKcal)
     }
     
-    private func presentView(kcal: Float) {
+    private func presentCircularBarView(kcal: Float) {
         
         let width = self.view.frame.width
         let height = self.view.frame.height
@@ -30,17 +34,18 @@ class ViewController: UIViewController {
         let pieChartView = PieChartView(frame: CGRect(x: 0, y: 0, width: width, height: height))
         pieChartView.center = self.view.center
         pieChartView.slices = [
-            Slice(percent: 0.2, color: UIColor.systemRed),
-            Slice(percent: 0.5, color: UIColor.systemBlue),
-            Slice(percent: 0.3, color: UIColor.systemGreen)
+            Slice(percent: 0.2, color: redColor),
+            Slice(percent: 0.5, color: blueColor),
+            Slice(percent: 0.3, color: greenColor)
         ]
         
         self.view.addSubview(pieChartView)
         
+        
         kcalText = UILabel().then {
             self.view.addSubview($0)
             $0.text = "\(kcal)"
-            $0.font = $0.font.withSize(50)
+            $0.font = .boldSystemFont(ofSize: 50)
             $0.snp.makeConstraints {
                 $0.center.equalTo(pieChartView)
             }
@@ -54,11 +59,15 @@ class ViewController: UIViewController {
 //                $0.width.equalTo(81.0)
 //                $0.height.equalTo(30.0)
                 $0.centerX.equalTo(kcalText)
-                $0.centerY.equalTo(kcalText).offset(30)
+                $0.centerY.equalTo(kcalText).offset(40)
                 
             }
         }
         
         pieChartView.animateChart()
+    }
+    
+    private func presentLineBarView() {
+        
     }
 }
